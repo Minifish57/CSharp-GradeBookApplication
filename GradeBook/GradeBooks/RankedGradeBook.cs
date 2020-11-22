8f923fa3044a6fdb23a2b5c6145a003a5c627c25
Minifish57 <anthony.fischer@hotmail.fr>
2020-11-22 22:46:50 +0100

    percent grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,47 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(this.Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            //get the number of students composing 20%
+            int student20Percent = this.Students.Count * 20 / 100;
+
+            List<double> orderedAverageGrade = new List<double>();
+            foreach (Student student in this.Students)
+            {
+                orderedAverageGrade.Add(student.AverageGrade);
+            }
+            orderedAverageGrade = orderedAverageGrade.OrderBy(a => a).ToList();
+
+            //get where the grade fit in the list
+            int index = orderedAverageGrade.FindIndex(g => g >= averageGrade) - 1;
+
+            if(index > 0 && index <= student20Percent)
+            {
+                return 'A';
+            }else if (index > student20Percent && index <= 2 * student20Percent)
+            {
+                return 'B';
+            }
+            else if (index > 2 * student20Percent && index <= 3 * student20Percent)
+            {
+                return 'C';
+            }
+            else if (index > 3 * student20Percent && index <= 4 * student20Percent)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
